08fe9447d4bb1f1a84aa664288350f358ec9d4a0
genbero <genbero@gmail.com>
2023-02-04 06:06:43 +0800

    修改page_view 样式

--- templates/init/src/components/page_view.js
+++ templates/init/src/components/page_view.js
@@ -1,10 +1,7 @@
-import {View,StyleSheet} from "react-native";
+import {View, StyleSheet} from "react-native";
+
 const PageView = (props) => {
-    return(
-        <View style={styles.container}>
-            {props.children}
-        </View>
-    )
+    return <View style={[styles.container, props.style]}>{props.children}</View>;
 }
 const styles = StyleSheet.create({
     container: {
